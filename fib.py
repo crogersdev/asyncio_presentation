b4045cf7e16273d387159902d0757ac0efdf1389
crogersdev <creedrogers@gmail.com>
2019-11-06 13:02:31 -0800

    generators with fib

--- fib.py
+++ fib.py
@@ -4,17 +4,23 @@ import asyncio
 import sys
 import time
 
+async def async_fib(n):
+    if n <= 1:
+        return 1
+    else:
+        return await async_fib(n - 1) + await async_fib(n - 2)
+
 def sync_fib(n):
     if n <= 1:
         return n
     else:
         return sync_fib(n - 1) + sync_fib(n - 2)
 
-async def async_fib(n):
+def gen_fib(n):
     if n <= 1:
-        return 1
+        yield 1
     else:
-        return await async_fib(n - 1) + await async_fib(n - 2)
+        yield gen_fib(n - 1) + gen_fib(n - 2)
 
 if __name__ == '__main__':
 
@@ -24,14 +30,19 @@ if __name__ == '__main__':
     if sync_async_switch == 'sync':
         start = time.time()
         sync_fib(fib_num)
-        print(f'elapsed: {time.time() - start} seconds')
+        print(f'sync_fib time elapsed: {time.time() - start} seconds')
+
+    elif sync_async_switch == 'gen':
+        start = time.time()
+        gen_fib(fib_num)
+        print(f'gen_fib time elapsed: {time.time() - start} seconds')
 
     elif sync_async_switch == 'async':
         loop = asyncio.get_event_loop()
         start = time.time()
         loop.run_until_complete(async_fib(fib_num))
-        print(f'elapsed: {time.time() - start} seconds')
-
+        async_fib(fib_num)
+        print(f'async_fib elapsed: {time.time() - start} seconds')
 
     else:
-        print('specify async or sync please')
+        print('specify gen, async or sync please')
